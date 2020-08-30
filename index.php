60e73466e15f221da53a2146c40d7bec56940bcf
adiansyahputra <adiansyahputraradit@gmail.com>
2020-08-31 03:02:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni itaque aliquam dolorem consequatur, aperiam exercitationem deserunt porro, cum facere nesciunt, iusto quas sapiente accusantium quisquam! Quasi incidunt aliquid ullam eligendi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
